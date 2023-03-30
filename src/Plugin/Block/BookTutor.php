479eb4717da607594bc8166636dca85bb2dfe8de
Ferdinand <afchike@gmail.com>
2023-03-30 18:35:52 +0100

    load current user from route context instead

--- src/Plugin/Block/BookTutor.php
+++ src/Plugin/Block/BookTutor.php
@@ -26,8 +26,17 @@ class BookTutor extends BlockBase {
  */
 
 public function build() {
-  $account = User::load(\Drupal::currentUser()->id());
-  $is_verified = !empty($account->get('field_id_verified')[0]->value);
+  //load the user in one line: $account = User::load(\Drupal::currentUser()->id());
+  //load the user id in two steps: $uid = \Drupal::currentUser()->id();
+  //$account = User::load($uid);
+  //$is_verified = !empty($account->get('field_id_verified')[0]->value);
+ /**
+   * Load the current user from route context instead.
+ */
+  $user = \Drupal::routeMatch()->getParameter('user');
+  $uid = $user->id();
+  $account = User::load($uid);
+  $is_verified = !empty($account->field_id_verified->value);
   return [
     '#theme' => 'book_tutor',
     '#verified' => $is_verified,
