bc4fe276261d4b358ce4daca778c0a640dbbd4cc
Ferdinand <afchike@gmail.com>
2023-05-11 21:31:19 +0100

    last updates

--- src/Plugin/Block/BookTutor.php
+++ src/Plugin/Block/BookTutor.php
@@ -33,14 +33,57 @@ public function build() {
  /**
    * Load the current user from route context instead.
  */
-  $user = \Drupal::routeMatch()->getParameter('user');
+$cacheability = new \Drupal\Core\Cache\CacheableMetadata();
+if ($user = \Drupal::routeMatch()->getParameter('user')) {
   $uid = $user->id();
   $account = User::load($uid);
   $is_verified = !empty($account->field_id_verified->value);
+  \Drupal::messenger()->addMessage(json_encode($account->field_first_name->value). 'first name value');
+  $first_name = !empty($account->field_first_name->value);
+  // Set the user role variable
+  $roles = $user->getRoles();
+  if(in_array('tutor', $roles)) {
+    $current_role = 'tutor';
+  }
+  // Add the user as a cache dependency in case the verified value changes.
+  $cacheability->addCacheableDependency($account);
+}
+$build = [
+  '#theme' => 'book_tutor',
+  '#verified' => $is_verified,
+  '#firstname' => $first_name,
+  '#current_role' => $current_role,
+  // Adding path as cache context because the user in the route parameter might
+  // change.
+  '#cache' => ['contexts' => ['url.path']]
+];
+// Apply the caching metadata to the build render array.
+$cacheability->applyTo($build);
+return $build;
+}
+}
+  
+
+
+
+
+
+/* refactored code
+  $current_role = NULL; // Or 'anonymous'.
+  $is_verified = FALSE;
+  // Now add all your checks.
+  if ($user = \Drupal::routeMatch()-->getParameter ('user')) {
+    $uid = $user->id();
+    $account = User::load($uid);
+    if ($account->hasField('field_id_verified') && !($account->get('field_id_verified')->isEmpty())) {
+    $is_verified = $account->field_id_verified->value;
+    }
+    if (in_array('tutor', $user->getRoles())) {
+      $current_role = 'tutor';
+    }
+  }
   return [
     '#theme' => 'book_tutor',
     '#verified' => $is_verified,
-  ];
-}
-
-}
+    '#current_role' => $current_role,
+  ]; */
